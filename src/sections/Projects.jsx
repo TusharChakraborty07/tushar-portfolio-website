0e3e3c61ce7580e83c44d797229a3891352a4912
TusharChakraborty07 <chakrabortytushar684@gmail.com>
2026-03-30 00:36:52 +0530

    add github links

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -138,10 +138,16 @@ const Projects = () => {
 
         {/* View All CTA */}
         <div className="text-center mt-12 animate-fade-in animation-delay-500">
-          <AnimatedBorderButton>
-            View All Projects
-            <ArrowUpRight className="w-5 h-5" />
-          </AnimatedBorderButton>
+          <a
+            href="https://github.com/TusharChakraborty07?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AnimatedBorderButton>
+              View All Projects
+              <ArrowUpRight className="w-5 h-5" />
+            </AnimatedBorderButton>
+          </a>
         </div>
       </div>
     </section>
